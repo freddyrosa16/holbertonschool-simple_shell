45737312afa5218e75053b41585090fa48506999
freddyrosa16 <freddy.rosa16@gmail.com>
2023-12-22 09:57:04 -0400

    Adding comment messages

--- shell.c
+++ shell.c
@@ -11,10 +11,13 @@ int main(void)
 	char **array;
 	pid_t child_pid;
 
+	/*Allocate memory for the command array*/
 	array = malloc(sizeof(char *) * 1024);
 
+	/*Main shell loop*/
 	while(1)
 	{
+		/*Display shell prompt if the input is from a terminal*/
 		if (isatty(STDIN_FILENO))
 		{
 			write(1, "#cisfun$ ", 9);
@@ -22,16 +25,19 @@ int main(void)
 		}
 		else
 		{
+			/*Read input if not from terminal*/
 			if (getline(&buf, &buf_size, stdin) == -1)
 			{
-				break;
+				break; /*Break the loop on the EOD*/
 			}
 		}
+		/*Trim newline character from the input*/
 		len = strlen(buf);
 		if (len > 0 && buf[len - 1] == '\n')
 		{
 			buf[len - 1] = '\0';
 		}
+		/*Tokenize the input into an array of strings*/
 		token = strtok(buf, " ");
 		i = 0;
 
@@ -41,10 +47,12 @@ int main(void)
 			token = strtok(NULL, "\t\n");
 			i++;
 		}
-		array[i] = NULL;
+		array[i] = NULL; /*Set the last element to NULL for execvp*/
 
+		/*Check for built-in commands: env and exit*/
 		if (strcmp(array[0], "env") == 0)
 		{
+			/*print enviroment variables*/
 			char **env_ptr = environ;
 			while (*env_ptr != NULL)
 			{
@@ -54,27 +62,32 @@ int main(void)
 		}
 		else if (strcmp(array[0], "exit") == 0)
 		{
+			/*Free allocated memory and exit the shell*/
 			free(array);
 			free(buf);
 			exit(0);
 		}
 		else
 		{
+			/*Fork a child process to execute external commands*/
 			child_pid = fork();
 
 			if (child_pid == 0)
 			{
+				/*Child process: executes the command*/
 				execvp(array[0], array);
-				perror("Error");
+				perror("Error"); /*Print an error message if execvp fails*/
 				exit(EXIT_FAILURE);
 			}
 			else
 			{
+				/*Parent process: wait for the child to finish*/
 				wait(&status);
 			}
 		}
-		i = 0;
+		i = 0; /*Reset array index*/
 	}
+	/*Free allocated memory before exiting*/
 	free(array);
 	free(buf);
 	return (0);
